67a37a7441b7c6c2869272fff0b39f9a49818253
Mitchell Reeves <m326r722@ku.edu>
2021-02-16 16:09:16 -0600

    More github test

--- battleship/ship.py
+++ battleship/ship.py
@@ -1,7 +1,8 @@
 #	Author: 
 #	Date: 
 import pygame
-
+from .constants import SQUARE_SIZE
+WHITE = (255,255,255)
 class Ship:
 
     def __init__(self, start_r, start_c, end_r, end_c):
@@ -29,7 +30,12 @@ class Ship:
             if self.horizontal:
                 self.ship.append(tuple((self.start_r, self.start_c + i, False)))
             else:
-                self.ship.append(tuple(self.start_r + i, self.start_c, False))
+                self.ship.append(tuple((self.start_r + i, self.start_c, False)))
 
-                
+
+    def draw(self, win):
+        square = SQUARE_SIZE
+        pygame.draw.rect(win, WHITE,(100,100,SQUARE_SIZE,SQUARE_SIZE))
+
+
         

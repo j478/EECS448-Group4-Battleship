b8c39400a8aa58249f597dbd50f37d59bd7cc316
Mason Hawks <mjhawks721@gmail.com>
2021-02-17 10:11:46 -0600

    definited init function for button

--- battleship/button.py
+++ battleship/button.py
@@ -2,7 +2,13 @@ import pygame
 
 class button():
     # initializes any variables needed for button
-    def __init__():
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
 
     # Creates the button on screen
     def draw():

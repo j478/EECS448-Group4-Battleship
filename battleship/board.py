bc2fcdc51702320d031b56f7ba8ff60049f429dc
UmarKhan11 <69403165+UmarKhan11@users.noreply.github.com>
2021-02-17 16:45:09 -0600

    Updated board
    I still need to do initialize game method. Rest of the methods should work

--- battleship/board.py
+++ battleship/board.py
@@ -3,12 +3,13 @@
 #   Start: 8:40 PM
 
 from . constants import *
+from . ship import *
 
 class Board:
     def __init__(self, win): #p0ships, p1ships
         #self.p0ships = p0ships
         #self.p1ships = p1ships
-        self.grid_width = 500 
+        self.grid_width = 500
         self.grid_height = 500 #buffer of 200 for the labels and gaps in between the two different grids
         self.win = win
         self.draw_background()
@@ -16,7 +17,7 @@ class Board:
     def draw_background(self):
         self.win.fill(BLACK)
         font = pygame.font.Font('freesansbold.ttf', 32) #https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
-        text = font.render('Select the location you wish to hit', True, WHITE, RED) #currently looks like trash, and  
+        text = font.render('Select the location you wish to hit', True, WHITE, RED) #currently looks like trash, and
         textRect = text.get_rect()
         textRect.center = (300,50)
         self.win.blit(text, textRect)
@@ -30,7 +31,32 @@ class Board:
             pygame.draw.line(self.win, WHITE, (0 + i * SQUARE_SIZE, HEIGHT - self.grid_height), ( 0 + i * SQUARE_SIZE, HEIGHT), 3)
             pygame.draw.line(self.win, WHITE, (self.grid_width + 200 + i * SQUARE_SIZE, HEIGHT - self.grid_height), (self.grid_width + 200 + i * SQUARE_SIZE, HEIGHT), 3)
 
+    def get_ship(self, ship):
+        print(ship.ship)
+        return ship.ship
+
+    def hit_ship(self, player, row, col, ship):
+        ships = self.get_ship(ship)
+        for list_row, list_col, list_state in ships:
+            if list_row == row and list_col == col:
+                self.draw(player, row, col, True)
+                print(f'Attacked ship at ({row}, {col})')
+                return
+        self.draw(player, row, col, False)
+        print(f'There was no ship at ({row}, {col})')
+
+    def draw(self, player, row, col, success):
+        if player == 0:
+            if success:
+                pygame.draw.rect(self.win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            else:
+                pygame.draw.rect(self.win, BROWN, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+        else:
+            if success:
+                pygame.draw.rect(self.win, RED, (row * SQUARE_SIZE + 700, col * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            else:
+                pygame.draw.rect(self.win, BROWN, (row * SQUARE_SIZE + 700, col * SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
-
-
+    def initialize_game(self):
+        pass

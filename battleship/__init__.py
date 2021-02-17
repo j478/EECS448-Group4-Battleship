c3680f070d2ed1bfb04f2598be5906dc1cf6f01a
Mason Hawks <mjhawks721@gmail.com>
2021-02-17 10:43:42 -0600

    mapped out in comments what init needs to do (draw window, update, moveship, isvalid, placepiece)

---
+++ battleship/__init__.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+# draw starting window
+# -window
+# -board
+# -buttons
+
+# some sort of update or on_button_press that constantly checks for when a button is pressed
+# -when button is pressed calls move ship function and passes in correct ship
+
+
+# move ship (ship)
+# -draws ship following cursor
+# -rotates based on key press
+# -creates an array based on mouse position and piece size and orientation
+# -if mouseclick and isValid then placePiece
+
+#isValid placement (takes array of inputs)
+# -if array is available on board returns true
+
+
+#placePiece (ship, location, orientation)
+# -finalizes piece placement

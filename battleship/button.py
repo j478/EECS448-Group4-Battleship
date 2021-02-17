34b706ff8549ec754fcebacc244c3784ebddc526
Mason Hawks <mjhawks721@gmail.com>
2021-02-17 10:09:09 -0600

    added undefined init, draw and hover functions and what they will do

---
+++ battleship/button.py
@@ -0,0 +1,11 @@
+import pygame
+
+class button():
+    # initializes any variables needed for button
+    def __init__():
+
+    # Creates the button on screen
+    def draw():
+
+    # returns a boolean of if the mouse is over the button
+    def hover():

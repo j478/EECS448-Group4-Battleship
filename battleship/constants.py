48856421b5a6fe268878815b9a414180cff23419
UmarKhan11 <69403165+UmarKhan11@users.noreply.github.com>
2021-02-18 01:42:55 -0600

    Update constants.py
    Added color of ships and comment for what each color specifies.

--- battleship/constants.py
+++ battleship/constants.py
@@ -4,10 +4,8 @@ WIDTH, HEIGHT = 1200, 600
 
 SQUARE_SIZE = 50
 
-BLACK = (0, 0, 0)
-GRAY = (128, 128, 128)
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
-BROWN = (160,82,45)
-LIGHT_GRAY = (32, 32, 32)
-GREEN = (0, 128, 0)
+BLACK = (0, 0, 0)           # Background color
+GRAY = (128, 128, 128)      # Default color of tile
+RED = (255, 0, 0)           # Missed an attack
+BLUE = (0, 0, 255)          # Ship 
+WHITE = (255, 255, 255)     # Grid line colors

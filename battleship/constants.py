97f8d2ea89f28956771eca5f427feca979b8cb13
UmarKhan11 <69403165+UmarKhan11@users.noreply.github.com>
2021-02-18 01:44:34 -0600

    Update constants.py
    Added green

--- battleship/constants.py
+++ battleship/constants.py
@@ -7,5 +7,6 @@ SQUARE_SIZE = 50
 BLACK = (0, 0, 0)           # Background color
 GRAY = (128, 128, 128)      # Default color of tile
 RED = (255, 0, 0)           # Missed an attack
-BLUE = (0, 0, 255)          # Ship 
+BLUE = (0, 0, 255)          # Ship
 WHITE = (255, 255, 255)     # Grid line colors
+GREEN = (0, 128, 0)         # Successfully hit enemy's ship

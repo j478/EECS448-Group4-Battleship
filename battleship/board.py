6af02cc5e8bb6a62d70b337eb837811053c9d659
UmarKhan11 <69403165+UmarKhan11@users.noreply.github.com>
2021-02-19 03:22:52 -0600

    Works but still need to fix up a couple things
    I'll modify the draw_grid() after everything else is working fine it that's alright.
    WORKS:
    hit_ship() works and draw() displays the current players ships and all attacks from both players. 
    DOES NOT WORK:
    In draw(), lines 78-85, I am not able to to loop through all the ships. (The commented parts). The uncommented lines display the ship, but it the wrong location when it is the right players turn.

--- battleship/board.py
+++ battleship/board.py
@@ -2,75 +2,67 @@
 #	Date: 2/14/2021
 #   Start: 8:40 PM
 
-from . constants import *
-from . ship import *
+from .constants import *
+from .ship import *
+
 
 class Board:
-    def __init__(self, win): #p0ships, p1ships
-        #self.p0ships = p0ships
-        #self.p1ships = p1ships
+    def __init__(self, win, player0ships, player1ships):  # p0ships, p1ships
+        # self.p0ships = p0ships
+        # self.p1ships = p1ships
         self.grid_width = 500
-        self.grid_height = 500 #buffer of 200 for the labels and gaps in between the two different grids
+        self.grid_height = 500  # buffer of 200 for the labels and gaps in between the two different grids
         self.win = win
         self.draw_background()
         ###could combine both of them into a single grid where you initialize say self.p0_hits_misses = [0,0,1,2, ....]
         ###where a 0 would be nothing has happened here, a 1 was a miss and a 2 was a hit. This would make for much
         ###easier printing
-        ###there is also not one user as there is player 0 and player 1. So, you would be designating one of the players as 
-        ###an enemy which is fine, but seems to be more confusing. There will only be one board class 
-        # user lists for hit and miss attacks
-        self.hit_u = []
-        self.miss_u = []
-        # enemy lists for hit and miss attacks
-        self.hit_e = []
-        self.miss_e = []
+        ###there is also not one user as there is player 0 and player 1. So, you would be designating one of the players as
+        ###an enemy which is fine, but seems to be more confusing. There will only be one board class
+        # 0 = nothing, 1 = hit, 2 = miss
+        self.p0_hit_misses = []
+        self.player0ships = player0ships
+        self.p1_hit_misses = []
+        self.player1ships = player1ships
 
     def draw_background(self):
         self.win.fill(BLACK)
-        font = pygame.font.Font('freesansbold.ttf', 32) #https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
-        text = font.render('Select the location you wish to hit', True, WHITE, RED) 
+        font = pygame.font.Font('freesansbold.ttf',
+                                32)  # https://www.geeksforgeeks.org/python-display-text-to-pygame-window/
+        text = font.render('Select the location you wish to hit', True, WHITE, RED)
         textRect = text.get_rect()
         # textRect.center = (300,50)
-        textRect.center = ((WIDTH /2) - 100, 50)
+        textRect.center = ((WIDTH / 2) - 100, 50)
         self.win.blit(text, textRect)
         ###constants here with the padding, explained in draw_grid
         pygame.draw.rect(self.win, GRAY, (0, 100, 500, 500))
-        pygame.draw.rect(self.win, GRAY, (700, 100, 500, 500)) #hardcoded of course
+        pygame.draw.rect(self.win, GRAY, (700, 100, 500, 500))  # hardcoded of course
         self.draw_grid()
 
     def draw_grid(self):
         for i in range(10):
             ###Would it be possible to make constants here. I.e. WIDTH_PADDING = 200, HEIGHT_PADDING = 100
-            ###I was also thinking it would be cool to have the grids like this: 
+            ###I was also thinking it would be cool to have the grids like this:
             ###  _| 1 | 2 | 3 | 4 | ...
             ###  A|
             ###  B|
-            ###  C| 
-            ###which would affect the padding but could also be accounted for with a constant where a box with label A would just be the 
+            ###  C|
+            ###which would affect the padding but could also be accounted for with a constant where a box with label A would just be the
             ###same size as a normal box
-            pygame.draw.line(self.win, WHITE, (0, HEIGHT - self.grid_height + i * SQUARE_SIZE), (self.grid_width, HEIGHT - self.grid_height + i * SQUARE_SIZE), 3 )
-            pygame.draw.line(self.win, WHITE, (WIDTH - self.grid_width, HEIGHT - self.grid_height + i * SQUARE_SIZE), (WIDTH, HEIGHT - self.grid_width + i *SQUARE_SIZE), 3 )
-            pygame.draw.line(self.win, WHITE, (0 + i * SQUARE_SIZE, HEIGHT - self.grid_height), ( 0 + i * SQUARE_SIZE, HEIGHT), 3)
-            pygame.draw.line(self.win, WHITE, (self.grid_width + 200 + i * SQUARE_SIZE, HEIGHT - self.grid_height), (self.grid_width + 200 + i * SQUARE_SIZE, HEIGHT), 3)
-    
-    ###dont need to pass the ships as these will already be stored in member variables. I commented them out for testing purposes but this 
-    ###is how it will interact with the Game class and get the location of the ships in the first place
-    def draw(self, player, ships):
-        ###this will just call itself and then return itself, I believe it only returns a single ship as well 
-        ships = self.get_ship(player, ships)
-        ###Unsure of what ships.ship would do 
-        list_of_ships = ships.ship
-        for row, col, state in list_of_ships:
-            if state == False:
-                if player == 0:
-                    pygame.draw.rect(self.win, BLUE, (row * SQUARE_SIZE + 700, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                else:
-                    pygame.draw.rect(self.win, BLUE, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-        self.draw_grid()
-        ###Basically I believe it shoudl go like this. 
+            pygame.draw.line(self.win, WHITE, (0, HEIGHT - self.grid_height + i * SQUARE_SIZE),
+                             (self.grid_width, HEIGHT - self.grid_height + i * SQUARE_SIZE), 3)
+            pygame.draw.line(self.win, WHITE, (WIDTH - self.grid_width, HEIGHT - self.grid_height + i * SQUARE_SIZE),
+                             (WIDTH, HEIGHT - self.grid_width + i * SQUARE_SIZE), 3)
+            pygame.draw.line(self.win, WHITE, (0 + i * SQUARE_SIZE, HEIGHT - self.grid_height),
+                             (0 + i * SQUARE_SIZE, HEIGHT), 3)
+            pygame.draw.line(self.win, WHITE, (self.grid_width + 200 + i * SQUARE_SIZE, HEIGHT - self.grid_height),
+                             (self.grid_width + 200 + i * SQUARE_SIZE, HEIGHT), 3)
+
+    def draw(self, player):
+        ###Basically I believe it shoudl go like this.
         ### draw_background()
         ###if player == 0:
-        ###     print that players ships on the right hand grid. 
+        ###     print that players ships on the right hand grid.
         ###     for ship in self.player0ships:
         ###         ship.draw()
         ###     for loc in player0_hits_misses:
@@ -80,56 +72,89 @@ class Board:
         ###             draw small red circle in center of square(which will go over top of the ship if there is one there)
         ###else:
         ### do for player 2
-        print(list_of_ships)
 
-    ###I do not believe this is necessary as a ship does not need to be cleared. It still needs to be displayed but it will just display that it 
-    ### is sunk. This will be done in the Ship class, but will display using ship.draw()
-    def clear_ships(self, player, ships):
-        ships = self.get_ship(player, ships)
-        list_of_ships = ships.ship
-        for row, col, state in list_of_ships:
-            if state == False:
-                if player == 0:
-                    pygame.draw.rect(self.win, GRAY, (row * SQUARE_SIZE + 700, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                else:
-                    pygame.draw.rect(self.win, GRAY, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-        self.draw_grid()
-        print(list_of_ships)
-    
+        self.draw_background()
+
+        if player == 0:
+            # for ship in self.player0ships.ship:
+            #     self.player0ships.ship.draw(self.win)
+            self.player0ships.draw(self.win)
+        else:
+            # for ship in self.player1ships.ship:
+            #     self.player0ships.ship.draw(self.win)
+            self.player1ships.draw(self.win)
+
+        for row, col, state in self.p0_hit_misses:
+            # miss
+            if state == 1:
+                center_x = row * SQUARE_SIZE + (SQUARE_SIZE / 2)
+                center_y = col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE + (SQUARE_SIZE / 2)
+                pygame.draw.circle(self.win, BLACK, (center_x, center_y), SQUARE_SIZE / 2)
+                # pygame.draw.rect(self.win, BLACK, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            # hit
+            elif state == 2:
+                center_x = row * SQUARE_SIZE + (SQUARE_SIZE / 2)
+                center_y = col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE + (SQUARE_SIZE / 2)
+                pygame.draw.circle(self.win, RED, (center_x, center_y), SQUARE_SIZE / 2)
+                # pygame.draw.rect(self.win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            else:
+                pass
+
+        for row, col, state in self.p1_hit_misses:
+            # miss
+            if state == 1:
+                center_x = row * SQUARE_SIZE + 700 + (SQUARE_SIZE / 2)
+                center_y = col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE +  (SQUARE_SIZE / 2)
+                pygame.draw.circle(self.win, BLACK, (center_x, center_y), SQUARE_SIZE / 2)
+                # pygame.draw.rect(self.win, BLACK, (
+                # row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            # hit
+            elif state == 2:
+                center_x = row * SQUARE_SIZE + 700 + (SQUARE_SIZE / 2)
+                center_y = col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE +  (SQUARE_SIZE / 2)
+                pygame.draw.circle(self.win, RED, (center_x, center_y), SQUARE_SIZE / 2)
+                # pygame.draw.rect(self.win, RED, (
+                # row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            else:
+                pass
+
     ###the GAME class would be attempting a hit on a square row and col, but it would not know if there is a ship there. So it can not pass ship
     ### hit_ship(self, player, row, col)
-    def hit_ship(self, player, row, col, ship):
-        ###first determine which player it is 
+    def hit_ship(self, player, row, col):
+        ###first determine which player it is
         ###then you may have to loop through the ships and their locations
-        ###if any of the ships contain a row,col == row and col that were passed. 
+        ###if any of the ships contain a row,col == row and col that were passed.
         ###     Then grab that ship and do ship.hit(row,col)
-        ###     mark a hit for that player in playerxhits_misses 
+        ###     mark a hit for that player in playerxhits_misses
         ###     return True
-        ###else 
+        ###else
         ###     mark a miss and return False
-        ships = self.get_ship(player, ship)
-        for list_row, list_col, list_state in ships.ship:
-            # hit enemies ship
-            if row == list_row and col == list_col:
-                ships.mark_hit(list_row, list_col)
-                if player == 0:
-                    pygame.draw.rect(self.win, GREEN, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                    self.hit_u.append((row, col))
-                else:
-                    pygame.draw.rect(self.win, GREEN, (row * SQUARE_SIZE + 700, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-                    self.hit_e.append((row + 700, col))
-                self.draw_grid()
-                return
-        # did not hit a ship
-        if player == 0:
-            pygame.draw.rect(self.win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            self.miss_u.append((row, col))
-        else:
-            pygame.draw.rect(self.win, RED, (row * SQUARE_SIZE + 700, col * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            self.miss_e.append((row + 700, col))
-        self.draw_grid()
 
-    ###this is not really relevant, if you already are passing the ship, then why run the function?
-    ### get_ship(self, player, row, col)
-    def get_ship(self, player, ship):
-        return ship
+        if player == 0:
+            for ship_row, ship_col, state in self.player1ships.ship:
+                if ship_row == row and ship_col == col:
+                    self.player0ships.mark_hit(row, col)
+                    self.p0_hit_misses.append(tuple((row, col, 2)))
+                    print(f'(PLAYER 0) Hit/Miss List: {self.p0_hit_misses}')
+                    print(f'(PLAYER 0) Successful attack at: ({row}, {col})')
+                    return
+            self.p0_hit_misses.append(tuple((row, col, 1)))
+            print(f'(PLAYER 0) Hit/Miss List: {self.p0_hit_misses}')
+            print(f'(PLAYER 0) Missed attack at: ({row}, {col})')
+        else:
+            for ship_row, ship_col, state in self.player0ships.ship:
+                if ship_row == row and ship_col == col:
+                    self.player1ships.mark_hit(row, col)
+                    self.p1_hit_misses.append(tuple((row, col, 2)))
+                    print(f'(PLAYER 1) Hit/Miss List: {self.p1_hit_misses}')
+                    print(f'(PLAYER 1) Successful attack at: ({row}, {col})')
+                    return
+            self.p1_hit_misses.append(tuple((row, col, 1)))
+            print(f'(PLAYER 1) Hit/Miss List: {self.p1_hit_misses}')
+            print(f'(PLAYER 1) Missed attack at: ({row}, {col})')
+
+    def info(self):
+        print(f'Player 0 ships: {self.player0ships.ship}')
+        print(f'Player 0 hit/misses: {self.p0_hit_misses}')
+        print(f'Player 1 ships: {self.player1ships.ship}')
+        print(f'Player 1 hit/misses: {self.p1_hit_misses}')

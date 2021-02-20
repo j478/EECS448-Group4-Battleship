182f9733c44ba11fba3c4a70bc8e49f9fd54d0c5
Mason Hawks <mjhawks721@gmail.com>
2021-02-20 10:18:23 -0600

    added player board in initialize and added functionality so that when a ship is clicked it follows cursor to board.

--- battleship/initialize.py
+++ battleship/initialize.py
@@ -1,12 +1,14 @@
 import pygame, sys
 
 from .button import Button
+from .constants import *
 
 class Initialize():
 
     def __init__(self, win):
         self.gameSizeSelected = False
-        
+        self.win = win
+
         b1 = Button((0,250,0), 0, 0, 500, 99, '1 Ship')
         b2 = Button((0,250,0), 0, 100, 500, 99, '2 Ship')
         b3 = Button((0,250,0), 0, 200, 500, 99, '3 Ship')
@@ -14,6 +16,7 @@ class Initialize():
         b5 = Button((0,250,0), 0, 400, 500, 99, '5 Ship')
         b6 = Button((0,250,0), 0, 500, 500, 99, '6 Ship')
 
+        
         self.placed1 = False
         self.placed2 = False
         self.placed3 = False
@@ -66,24 +69,25 @@ class Initialize():
         s5 = Button((0,250,0), 0, 400, 500, 50, 'Battle Ship')
         s6 = Button((0,250,0), 0, 500, 600, 50, 'Carrier')
         pygame.draw.rect(win,(0,0,0),(0,0,1300, 800),0)
+        self.drawPlayerBoard(win)
         
         for i in range(shipCount+1):
-            if i is 1: 
+            if i == 1: 
                 s1.draw(win)
-            
-            if i is 2:
+                
+            if i == 2:
                 s2.draw(win)
 
-            if i is 3:
+            if i == 3:
                 s3.draw(win)
 
-            if i is 4: 
+            if i == 4: 
                 s4.draw(win)
 
-            if i is 5: 
+            if i == 5: 
                 s5.draw(win)
 
-            if i is 6:
+            if i == 6:
                 s6.draw(win)
 
         while self.shipsSelected < shipCount:
@@ -91,10 +95,11 @@ class Initialize():
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos = pygame.mouse.get_pos()
-                    if s1.hover(pos) == True and shipCount >= 1 and self.placed1 is False:
+                    if s1.hover(pos) == True and shipCount >= 1 and self.placed1 == False:
                         s1 = Button((64,64,64), 0, 0, 100, 50, 'Jet ski')
                         s1.draw(win)
                         pygame.display.update()
+                        self.placeShip(1,win)
                         self.placed1 = True
                         self.shipsSelected += 1
 
@@ -136,4 +141,47 @@ class Initialize():
                         self.placed6 = True
                         self.shipsSelected += 1
 
-                    
+    def placeShip (self, shipNum,win):
+        self.shipPlaced = False
+        while self.shipPlaced is False:
+            for event in pygame.event.get():
+                self.drawPlayerBoard(win)
+                pos = pygame.mouse.get_pos()
+                pygame.draw.rect(win,(70, 70, 70),(pos[0],pos[1],50*shipNum, 50))
+                pygame.display.update()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    print('mouse clicked')
+                    self.shipPlace = True
+
+
+    def drawPlayerBoard (self,win):
+        self.win.fill(BLACK)
+        #pygame.draw.rect(self.win, BLUE, (LEFT_PADDING, TOP_PADDING, GRID_WIDTH, GRID_HEIGHT))
+        pygame.draw.rect(self.win, BLUE, (LEFT_PADDING + GRID_WIDTH + MIDDLE_PADDING, TOP_PADDING, GRID_WIDTH, GRID_HEIGHT))  
+        for i in range(11):
+            # (win, color, (start X, start Y) , (end X, end Y))
+            #horizontals
+            pygame.draw.line(self.win, WHITE, (WIDTH - RIGHT_PADDING - GRID_WIDTH, TOP_PADDING + i * SQUARE_SIZE + 50),
+                                              (WIDTH - RIGHT_PADDING, TOP_PADDING + i * SQUARE_SIZE + 50))
+            #verticals
+            pygame.draw.line(self.win, WHITE, (WIDTH - RIGHT_PADDING - GRID_WIDTH + i * SQUARE_SIZE + 50, TOP_PADDING),
+                                              (WIDTH - RIGHT_PADDING - GRID_WIDTH + i * SQUARE_SIZE + 50, HEIGHT - BOTTOM_PADDING))
+
+            if i != 0:
+                font = pygame.font.Font('freesansbold.ttf', 32)
+                txt = "" + str(i)
+                text = font.render(txt, True, BLACK, BLUE)
+                textRect = text.get_rect()
+                
+                #numbers in top row
+                textRect.center = (LEFT_PADDING + GRID_WIDTH + MIDDLE_PADDING + i * SQUARE_SIZE + SQUARE_SIZE // 2, TOP_PADDING + SQUARE_SIZE // 2)
+                self.win.blit(text, textRect)
+
+                #letters on leftmost column
+                txt = chr(64 + i)
+                text = font.render(txt, True, BLACK, BLUE)
+                textRect.center = (LEFT_PADDING + GRID_WIDTH + MIDDLE_PADDING + SQUARE_SIZE // 2, TOP_PADDING + i * SQUARE_SIZE + SQUARE_SIZE // 2)
+                self.win.blit(text, textRect)
+                #65 - 74 65 = A, 74 = J from https://stackoverflow.com/questions/4528982/convert-alphabet-letters-to-number-in-python
+
+

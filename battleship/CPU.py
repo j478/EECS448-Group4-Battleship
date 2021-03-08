512afe1257be726d2993271b47c91bbb91423f89
j478 <78564427+j478@users.noreply.github.com>
2021-03-08 13:56:00 -0600

    Update CPU.py

--- battleship/CPU.py
+++ battleship/CPU.py
@@ -1,4 +1,14 @@
 import pygame
 from .constants import *
 
+# When placing ships, randomly generate rows and columns.
+# CPU should have a %age of a chance that it will place a ship horizontially
+# CPU will recieve a difficulty level upon initialization
+# CPU will then select shots based on that difficulty level
+
+# member variables:
+# needs to track if a previous shot was a hit or not
+# needs to track if a ship has been sunk or not
+# needs to be able to see the other player's ship locations(hard mode)
+
 Class CPU:

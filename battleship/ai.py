936f13da5b1b88983d78f4670ae5c1b82a8c4be7
0215karenliu <74805992+0215karenliu@users.noreply.github.com>
2021-03-08 13:21:17 -0600

    Create ai.py

---
+++ battleship/ai.py
@@ -0,0 +1,48 @@
+import pygame
+
+class AI:
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    # spit out a random number from 1 to 10
+    def random_number():
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    # spit out h or v
+    def random_rotation():
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    # return true if hit, false if not
+    def is_hit():
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    #take row from random number_generator
+    #take col from random number_generator
+    #take rotation from random random_rotation
+    # fire at that position
+    def easy_mode():
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    # do what easy_mode does until a spot is hit
+    def med_mode():
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    # read the player's board and attak
+    def hard_mode():
+    # @pre -
+    # @param -
+    # @post -
+    # @return -
+    # AI place ship at the begining of the game
+    def place_ship():

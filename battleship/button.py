9235aca34465aae85179215a6e11388901a515c7
0215karenliu <74805992+0215karenliu@users.noreply.github.com>
2021-03-10 11:55:37 -0600

    Update button.py

--- battleship/button.py
+++ battleship/button.py
@@ -5,13 +5,9 @@ pygame.init()
 
 class Button:
     # @pre - none
-    #
     # @param - color of button, position, dimensions and text
-    #
     # @post - defines the passed in variables
-    #
     # @return - none
-    #
     def __init__(self, color, x, y, width, height, text=''):
         self.color = color
         self.x = x
@@ -21,13 +17,9 @@ class Button:
         self.text = text
 
     # @pre - the button needs to be defined
-    #
     # @param - window to draw on
-    #
     # @post - draws the button
-    #
     # @return - none
-    #
     def draw(self, win):
         # draws button
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
@@ -39,13 +31,9 @@ class Button:
             self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
 
     # @pre - button needs to be defined
-    #
     # @param - position of mouse
-    #
     # @post - checks if mouse is over button
-    #
     # @return - boolean of whether over position
-    #
     def hover(self, pos):
         if pos[0] > self.x and pos[0] < self.x + self.width:
             if pos[1] > self.y and pos[1] < self.y + self.height:

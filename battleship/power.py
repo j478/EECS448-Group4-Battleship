bef9ef4c3f03b57c4caab03e6954cfe255e111a4
0215karenliu <74805992+0215karenliu@users.noreply.github.com>
2021-03-10 11:50:42 -0600

    Update power.py

--- battleship/power.py
+++ battleship/power.py
@@ -1,8 +1,6 @@
 class Power:
     # @pre - Difficulty selected.
-    # @param row - Row of power-up.
-    # @param col - Column of power-up.
-    # @param difficulty - Integer. Easy (0), medium (1), or hard (2).
+    # @param - row for row, col for column, difficulty (int) Easy (0), medium (1), or hard (2).
     # @post - None
     # @return -None
     def __init__(self, row, col, difficulty):
